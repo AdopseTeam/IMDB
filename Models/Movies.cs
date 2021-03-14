01cde7ab98cdf80f2e92c32b831a343dc54e5cb4
Giapa <giapa@protonmail.com>
2021-03-14 11:50:57 +0200

    Change model and update views

--- Models/Movies.cs
+++ Models/Movies.cs
@@ -9,6 +9,6 @@ namespace MvcMovie.Models{
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
-        public decimal Price { get; set; }
+        public decimal Rating { get; set; }
     }
 }
