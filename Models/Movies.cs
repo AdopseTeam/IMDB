37be61a1f443412e468e0038b05c1c8211bdf2a9
Giapa <giapa@protonmail.com>
2021-03-14 13:01:36 +0200

    Add title search support

--- Models/Movies.cs
+++ Models/Movies.cs
@@ -1,11 +1,13 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models{
     public class Movie{
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
