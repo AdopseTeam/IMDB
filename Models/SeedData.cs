5da0bedebe74e5066e6add55f9fa64d91b555ee6
Giapa <giapa@protonmail.com>
2021-04-01 21:53:57 +0300

    Add api seeding for series

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -82,6 +82,16 @@ namespace MvcSeries.Models
     {
         public static void Initialize(IServiceProvider serviceProvider)
         {
+            const string GURL = "https://api.themoviedb.org/3/genre/tv/list";
+            string GurlParameters = "?api_key=<api_token>=en-US";
+            var genreResponse = HTTP.Response.returnResponse(GURL, GurlParameters);
+            JArray genrejObject = (JArray)genreResponse["genres"];
+
+            const string URL = "https://api.themoviedb.org/3/tv/popular";
+            string urlParameters = "?api_key=<api_token>&language=en-US&page=1";
+            var seriesReponse = HTTP.Response.returnResponse(URL, urlParameters);
+            JArray seriesObject = (JArray)seriesReponse["results"];
+
             using (var context = new MvcSeriesContext(
                 serviceProvider.GetRequiredService<
                     DbContextOptions<MvcSeriesContext>>()))
@@ -90,35 +100,26 @@ namespace MvcSeries.Models
                 if (context.Series.Any())
                 {
                     return;   // DB has been seeded
-                }
-
-                context.Series.AddRange(
-                    new Series
-                    {
-                        Title = "Two and a half men",
-                        ReleaseDate = DateTime.Parse("2003-2-12"),
-                        Genre = "Comedy",
-                        Seasons = 5,
-                        Rating = 7.5M
-                    },
-
-                    new Series
-                    {
-                        Title = "See ",
-                        ReleaseDate = DateTime.Parse("2019-3-13"),
-                        Genre = "Action",
-                        Seasons = 2,
-                        Rating = 8.9M
-                    },
-
-                    new Series
-                    {
-                        Title = "After life",
-                        ReleaseDate = DateTime.Parse("2018-2-23"),
-                        Genre = "Drama",
-                        Rating = 6
+                } else {
+                    // Loop through the series
+                    foreach(var item in seriesObject.Children()){
+                        var genre_id = (string)item["genre_ids"].First;
+                        // Loop through the genres to find the matching id
+                        foreach(var genreObj in genrejObject){
+                            if((string)genreObj["id"] == genre_id){
+                                var genre = (string)genreObj["name"];
+                                context.Series.Add(
+                                new Series{
+                                    Id = (int)item["id"],
+                                    Title = (string)item["original_name"],
+                                    ReleaseDate = DateTime.Parse((string)item["first_air_date"]),
+                                    Genre = genre,
+                                    Rating = (int)item["vote_average"],
+                                }
+                        );
+                            }
+                        }
                     }
-                );
                 context.SaveChanges();
             }
         }
