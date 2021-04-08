2cffe835ed79a53e9e29d4fc14718c62db2534a8
Giapa <giapa@protonmail.com>
2021-04-08 20:40:16 +0300

    Change setup to enable electron

--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
-using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-// using MvcMovie.Models;
-// using MvcActor.Models;
-// using MvcSeries.Models;
 using ElectronNET.API;
 
 namespace IMDB
@@ -17,17 +11,12 @@ namespace IMDB
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static string HostPort => Environment.GetEnvironmentVariable("PORT")??"5000";
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseElectron(args);
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseKestrel(options =>
-                    {
-                        options.ListenAnyIP(Int32.Parse(HostPort));
-                    });
                 });
     }
 }
