67096825a008934fb5f3dae8e546f0aaabea7aa5
savvas23 <savsal23@gmail.com>
2021-04-16 15:31:27 +0300

    Add missing details to Actor

--- Data/MvcActorContext.cs
+++ Data/MvcActorContext.cs
@@ -3,6 +3,7 @@ using MvcActor.Models;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Linq;
+using System.Timers;
 
 namespace MvcActor.Data
 {
@@ -27,6 +28,7 @@ namespace MvcActor.Data
                 string[] names = ((string)item["name"]).Split(" ");
                 modelBuilder.Entity<Actor>().HasData(
                     new Actor{
+                        ActorId= (int)item["id"],
                         Id = counter,
                         LastName = names[names.Length -1],
                         FirstName = String.Join(" ", names.Take(names.Length-1)),
@@ -34,8 +36,36 @@ namespace MvcActor.Data
                 );
                 counter += 1;
             }
+
+            JArray actorDetails = new JArray();
+            Timer ApiCallTimer;
+           
+            foreach (var item in Actor)   
+            {
+                string detailsURL = $"https://api.themoviedb.org/3/person/{item.ActorId}";
+                string detailsUrlParameters = $"?api_key={Environment.GetEnvironmentVariable("API")}&language=en-US";
+                var detailsResponse = HTTP.Response.returnResponse(detailsURL, detailsUrlParameters);
+                actorDetails =(JArray)detailsResponse["details"];
+                foreach (var detail in actorDetails) {
+                    modelBuilder.Entity<Actor>().HasData(
+                        item.Bio = (string)detail["biography"],
+                        item.Birthday=(DateTime)detail["birthday"],
+                        item.Profile_pic_path=(string)detail["profile_path"]
+                        ) ;
+                }
+                SetApiTimer();   //Use timer to call API every 100ms to avoid getting blocked from it.
+            }
+            void SetApiTimer()
+            {
+                // Create a timer with a 100ms interval.
+                ApiCallTimer = new Timer(100);
+                ApiCallTimer.AutoReset = true;
+                //ApiCallTimer.Enabled = true;
+                return;
+            }
         }
         public DbSet<Actor> Actor { get; set; }
+
 
     }
 }
--- Models/Actor.cs
+++ Models/Actor.cs
@@ -5,6 +5,9 @@ namespace MvcActor.Models
 {
     public class Actor
     {
+        public int ActorId { get; set; }
+        public string Bio { get; set; }
+        public string Profile_pic_path { get; set; }
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
