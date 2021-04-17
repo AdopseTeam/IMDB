5fad7b83c33e3ab04049a27538c84f51ddb01cf1
savvas23 <savsal23@gmail.com>
2021-04-17 22:24:43 +0300

    Added Movie overview & poster to model+api data

--- Models/Movies.cs
+++ Models/Movies.cs
@@ -5,7 +5,8 @@ namespace MvcMovie.Models{
     public class Movie{
         public int Id { get; set; }
         public string Title { get; set; }
-
+        public string Overview { get; set; }
+        public string Poster_path { get; set; }
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
