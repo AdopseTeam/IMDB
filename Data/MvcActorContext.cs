c8152e9c24ef64290bb0b301eed416fbb8050724
savvas23 <savsal23@gmail.com>
2021-04-17 11:51:11 +0300

    Actor details refactor

--- Data/MvcActorContext.cs
+++ Data/MvcActorContext.cs
@@ -17,7 +17,9 @@ namespace MvcActor.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder){
             JArray actorsObject = new JArray();
-            for(int i=1; i<20; i++){
+            JArray actorDetails = new JArray();
+            string detailsUrlParameters = $"?api_key={Environment.GetEnvironmentVariable("API")}&language=en-US";
+            for (int i=1; i<20; i++){
                 const string URL = "https://api.themoviedb.org/3/person/popular";
                 string urlParameters = $"?api_key={Environment.GetEnvironmentVariable("API")}&language=en-US&page={i}";
                 var seriesReponse = HTTP.Response.returnResponse(URL, urlParameters);
@@ -37,32 +39,25 @@ namespace MvcActor.Data
                 counter += 1;
             }
 
-            JArray actorDetails = new JArray();
-            Timer ApiCallTimer;
+            
+            
            
             foreach (var item in Actor)   
             {
                 string detailsURL = $"https://api.themoviedb.org/3/person/{item.ActorId}";
-                string detailsUrlParameters = $"?api_key={Environment.GetEnvironmentVariable("API")}&language=en-US";
+                
                 var detailsResponse = HTTP.Response.returnResponse(detailsURL, detailsUrlParameters);
                 actorDetails =(JArray)detailsResponse["details"];
                 foreach (var detail in actorDetails) {
-                    modelBuilder.Entity<Actor>().HasData(
+                    
                         item.Bio = (string)detail["biography"],
                         item.Birthday=(DateTime)detail["birthday"],
                         item.Profile_pic_path=(string)detail["profile_path"]
-                        ) ;
+                       
                 }
-                SetApiTimer();   //Use timer to call API every 100ms to avoid getting blocked from it.
+               
             }
-            void SetApiTimer()
-            {
-                // Create a timer with a 100ms interval.
-                ApiCallTimer = new Timer(100);
-                ApiCallTimer.AutoReset = true;
-                //ApiCallTimer.Enabled = true;
-                return;
-            }
+          
         }
         public DbSet<Actor> Actor { get; set; }
 
