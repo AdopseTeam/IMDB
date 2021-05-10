b3a4be1be048d2c880479dab95afa69b395319ac
movariemai <57597993+movariemai@users.noreply.github.com>
2021-05-10 15:19:51 +0300

    Add ReleasedOnThisDay page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,54 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using IMDB.Models;
+using MvcMovie.Models;
+using MvcMovie.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace IMDB.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly MvcMovieContext _context;
+        public static DateTime Today { get; }
+        
+        public HomeController(ILogger<HomeController> logger, MvcMovieContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ReleasedOnThisDay(string searchString)
+        {
+
+                var movies = from m in _context.Movie
+                        select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                string lowerMovie = searchString.ToLower();
+                movies = movies.Where(s => s.Title.ToLower().Contains(lowerMovie));
+            }
+
+            return View(await movies.ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
