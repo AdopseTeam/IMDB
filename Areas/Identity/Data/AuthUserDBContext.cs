3eac118ceb8eaa946b80f9d40c326ec18cdca787
Gregory Giapa <giapa@protonmail.com>
2021-05-12 20:54:36 +0300

    Update AuthUserDBContext.cs
    Removes seeders for series

--- Areas/Identity/Data/AuthUserDBContext.cs
+++ Areas/Identity/Data/AuthUserDBContext.cs
@@ -144,7 +144,6 @@ namespace IMDB.Data
 
             this.seedActors(builder);
             this.seedMovies(builder);
-            this.seedSeries(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
