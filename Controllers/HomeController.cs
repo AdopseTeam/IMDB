b73f628ac63b324df4c3dc195c9eeaf2e30e76e2
Σάββας Σαλαμπάσης <63872837+savvas23@users.noreply.github.com>
2021-05-15 17:20:58 +0300

    Additional commit to MR#74 (#76)
    * Add ReleasedOnThisDay page
    
    * Add functionality for Released On This Month
    
    * Add carousel with 3 items per slide
    
    * Change to ReleasedOnThisMonth
    
    * Homepage Carousel changes
    
    * some css changes"
    
    * Glide js slider for responsiveness
    
    Co-authored-by: movariemai <57597993+movariemai@users.noreply.github.com>

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,55 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using IMDB.Models;
+using MvcMovie.Models;
+using MvcMovie.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace IMDB.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MvcMovieContext _released;
 
-        public HomeController(ILogger<HomeController> logger)
+        
+        public HomeController(ILogger<HomeController> logger, MvcMovieContext released)
         {
             _logger = logger;
+            _released = released;
         }
-
-        public IActionResult Index()
+        
+        public async Task<IActionResult> Index()
+        {
+            int currentMonth = DateTime.Now.Month;
+            
+                var movies = from m in _released.Movies.Where(m=>(m.ReleaseDate).Month==currentMonth)
+                    select m;
+               
+                
+            return View(await movies.Take(18).ToListAsync());
+        }
+        
+        public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> ReleasedOnThisMonth()
+        {
+            int currentMonth = DateTime.Now.Month;
+
+            var movies = from m in _released.Movies.Where(m => (m.ReleaseDate).Month == currentMonth)
+                         select m;
+
+            return View(await movies.ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
