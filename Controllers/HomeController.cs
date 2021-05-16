3e0a4da1c75ea6487d5f3fac28aca8137accc423
Gregory Giapa <giapa@protonmail.com>
2021-05-16 13:01:17 +0300

    Simplify homepage and fix controller (#81)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,53 +3,43 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using IMDB.Models;
-using MvcMovie.Models;
 using MvcMovie.Data;
-using MvcActor.Models;
 using MvcActor.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Dynamic;
 
 namespace IMDB.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly MvcMovieContext _released;
+        private readonly MvcMovieContext _movie;
 
-        private readonly MvcActorContext _born;
+        private readonly MvcActorContext _actor;
 
-        public HomeController(ILogger<HomeController> logger, MvcMovieContext released, MvcActorContext born)
+        public HomeController(ILogger<HomeController> logger, MvcMovieContext movie, MvcActorContext actor)
         {
             _logger = logger;
-            _released = released;
-            _born = born;
+            _movie = movie;
+            _actor = actor;
         }
-        
-        public async Task<IActionResult> Index()
+
+        public IActionResult Index()
         {
-            int currentMonth = DateTime.Now.Month;
-            
-                var movies = from m in _released.Movies.Where(m=>(m.ReleaseDate).Month==currentMonth)
-                    select m;
-               
-                
-            return View(await movies.Take(18).ToListAsync());
-        }
-        
-        public IActionResult Privacy()
-        {
-            return View();
+            var movies = from m in _movie.Movies select m;
+            var actors = from a in _actor.Actor select a;
+            dynamic mymodel = new ExpandoObject();
+            mymodel.Movie = movies;
+            mymodel.Actor = actors;
+            return View(mymodel);
         }
 
         public async Task<IActionResult> ReleasedOnThisMonth()
         {
-            int currentMonth = DateTime.Now.Month;
-
-            var movies = from m in _released.Movies.Where(m => (m.ReleaseDate).Month == currentMonth)
+            var movies = from m in _movie.Movies.Where(m => (m.ReleaseDate).Month == DateTime.Now.Month)
                          select m;
 
             return View(await movies.ToListAsync());
@@ -57,22 +47,20 @@ namespace IMDB.Controllers
 
         public async Task<IActionResult> TopMovies()
         {
-            var movies = from m in _released.Movies.Where(m => m.Rating >= 8) orderby m.Rating descending
+            var movies = from m in _movie.Movies.Where(m => m.Rating >= 8) orderby m.Rating descending
                         select m;
-            
+
             return View(await movies.ToListAsync());
         }
 
         public async Task<IActionResult> BornThisMonth()
         {
-            int currentMonth = DateTime.Now.Month;
-
-            var actors = from a in _born.Actor.Where(a => (a.Birthday).Month == currentMonth) orderby (a.Birthday).Day
+            var actors = from a in _actor.Actor.Where(a => (a.Birthday).Month == DateTime.Now.Month) orderby (a.Birthday).Day
                     select a;
 
             return View(await actors.ToListAsync());
         }
-        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
